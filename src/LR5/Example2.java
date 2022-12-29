4f4b73bbabde3a7d79427b2db3c072a13f9018e1
shinkar24 <vladikadikvd@mail.ru>
2022-12-29 19:41:38 +0500

    lr 5

--- src/LR5/Example2.java
+++ src/LR5/Example2.java
@@ -7,20 +7,13 @@ public class Example2 {
         myexample.showCharArray();
     }
     static class Example {
-        // классом, у которого есть два символьных поля и метод.
         private char ch1;
         private char ch2;
-
-        //Один из методов позволяет присвоить значение полю.
         public void Setch(char ch1, char ch2) {
             this.ch1 = ch1;
             this.ch2 = ch2;
         }
-
-        //Он возвращает результат, и у него нет аргументов.
         public void showCharArray() {
-        /*При вызове метод выводит в консольное окно все символы из кодовой таблицы, которые находятся «между» символами,
-являющимися значениями полей объекта (из которого вызывается метод*/
             int int1 = ch1;
             int int2 = ch2;
             if (ch1 > ch2) {
